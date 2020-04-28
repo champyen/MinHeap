a6a7d00d9fd7f53b81e61fefd6d75d494158d9fd
Champ Yen <champ.yen@gmail.com>
2020-04-28 09:27:13 +0800

    fix ident

--- min_heap.cpp
+++ min_heap.cpp
@@ -29,7 +29,7 @@ template <typename T>
 void MinHeap<T>::makeHeap()
 {
     for(int i = size/2; i >= 0; i--){
-		heapify(i);
+        heapify(i);
     }
 }
 
@@ -40,12 +40,12 @@ void MinHeap<T>::heapify(int i)
     int left = MH_LEFT(i);
     int right = MH_RIGHT(i);
     if(left < size){
-		if(heap[left]->kval < heap[min]->kval)
-			min = left;
+        if(heap[left]->kval < heap[min]->kval)
+            min = left;
     }
     if(right < size){
-		if(heap[right]->kval < heap[min]->kval)
-			min = right;
+        if(heap[right]->kval < heap[min]->kval)
+            min = right;
     }
 
     if(min != i){
@@ -78,42 +78,42 @@ int MinHeap<T>::pop(shared_ptr<T>node)
 class Node
 {
 public:
-	Node(int key) {kval = key;}
-	~Node() {};
-	unsigned int kval;
+    Node(int key) {kval = key;}
+    ~Node() {};
+    unsigned int kval;
 };
 
-#define TEST_SIZE	16
+#define TEST_SIZE   16
 
 int main(void)
 {
-	MinHeap<Node> heap(TEST_SIZE);
-	srand(time(NULL));
-	for(int i = 0; i < TEST_SIZE; i++){ 
-		shared_ptr<Node> n = make_shared<Node>(rand()&0xFF);
-		printf("%u\n", n->kval);
-		heap.insert(n);
-	}
-	heap.makeHeap();
-	printf("\n");
+    MinHeap<Node> heap(TEST_SIZE);
+    srand(time(NULL));
+    for(int i = 0; i < TEST_SIZE; i++){ 
+        shared_ptr<Node> n = make_shared<Node>(rand()&0xFF);
+        printf("%u\n", n->kval);
+        heap.insert(n);
+    }
+    heap.makeHeap();
+    printf("\n");
 
-	for(int i = 0; i < 1024; i++){
-		shared_ptr<Node> n = heap.getMin();
-		int min = n->kval;
+    for(int i = 0; i < 128; i++){
+        shared_ptr<Node> n = heap.getMin();
+        int min = n->kval;
 
-		n = make_shared<Node>((rand()&0xFF) + min);
-		heap.pop(n);
-		printf("%u %u\n", min, n->kval);
-	}
+        n = make_shared<Node>((rand()&0xFF) + min);
+        heap.pop(n);
+        printf("%u %u\n", min, n->kval);
+    }
 
-	printf("\n");
-	for(int i = 0; i < TEST_SIZE; i++){
-		shared_ptr<Node>n = heap.getMin();
-		heap.pop(nullptr);
-		printf("%u\n", n->kval);
-	}
+    printf("\n");
+    for(int i = 0; i < TEST_SIZE; i++){
+        shared_ptr<Node>n = heap.getMin();
+        heap.pop(nullptr);
+        printf("%u\n", n->kval);
+    }
 
-	return 0;
+    return 0;
 }
 
 #endif
