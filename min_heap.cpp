a02e78eab854c2870852a774258eed09326b19b5
Champ Yen <champ.yen@gmail.com>
2020-04-28 09:19:24 +0800

    1. fix makeHeap 2. used shared_ptr instead of raw pointer

--- min_heap.cpp
+++ min_heap.cpp
@@ -1,11 +1,11 @@
-
+#include <stdio.h>
 #include "min_heap.h"
 
 template <typename T> 
-MinHeap<T>::MinHeap(int cap)
+MinHeap<T>::MinHeap(int capacity)
 {
-    heap = new T*[cap];
-    this.cap = cap;
+    heap = new shared_ptr<T>[capacity];
+    cap = capacity;
 }
 
 template <typename T> 
@@ -16,7 +16,7 @@ MinHeap<T>::~MinHeap()
 }
 
 template <typename T>
-bool MinHeap<T>::insert(T* item)
+bool MinHeap<T>::insert(shared_ptr<T> item)
 {
     if(size < cap){
         heap[size++] = item;
@@ -29,21 +29,7 @@ template <typename T>
 void MinHeap<T>::makeHeap()
 {
     for(int i = size/2; i >= 0; i--){
-        int min = i;
-
-        int left = MH_LEFT(i);
-        int right = MH_RIGHT(i);
-        if(left < size && heap[left]->kval < heap[i]->kval)
-            min = left;
-        if(right < size && heap[right]->kval < heap[min]->kval)
-            min = right;
-
-        if(min != i){
-            T *tmp;
-            tmp = heap[i];
-            heap[i] = heap[min];
-            heap[min] = tmp;
-        }
+		heapify(i);
     }
 }
 
@@ -53,15 +39,17 @@ void MinHeap<T>::heapify(int i)
     int min = i;
     int left = MH_LEFT(i);
     int right = MH_RIGHT(i);
-    if(left < size && heap[left]->kval < heap[min]->kval){
-        min = left;
+    if(left < size){
+		if(heap[left]->kval < heap[min]->kval)
+			min = left;
     }
-    if(right < size && heap[right]->kval < heap[min]->kval){
-        min = right;
+    if(right < size){
+		if(heap[right]->kval < heap[min]->kval)
+			min = right;
     }
 
     if(min != i){
-        T *tmp;
+        shared_ptr<T>tmp;
         tmp = heap[i];
         heap[i] = heap[min];
         heap[min] = tmp;
@@ -70,7 +58,7 @@ void MinHeap<T>::heapify(int i)
 }
 
 template <typename T>
-int MinHeap<T>::pop(T *node)
+int MinHeap<T>::pop(shared_ptr<T>node)
 {
     if(node == nullptr && size > 0){
         size--;
@@ -81,3 +69,51 @@ int MinHeap<T>::pop(T *node)
     heapify(0);
     return size;
 }
+
+#ifdef TEST
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+class Node
+{
+public:
+	Node(int key) {kval = key;}
+	~Node() {};
+	unsigned int kval;
+};
+
+#define TEST_SIZE	16
+
+int main(void)
+{
+	MinHeap<Node> heap(TEST_SIZE);
+	srand(time(NULL));
+	for(int i = 0; i < TEST_SIZE; i++){ 
+		shared_ptr<Node> n = make_shared<Node>(rand()&0xFF);
+		printf("%u\n", n->kval);
+		heap.insert(n);
+	}
+	heap.makeHeap();
+	printf("\n");
+
+	for(int i = 0; i < 1024; i++){
+		shared_ptr<Node> n = heap.getMin();
+		int min = n->kval;
+
+		n = make_shared<Node>((rand()&0xFF) + min);
+		heap.pop(n);
+		printf("%u %u\n", min, n->kval);
+	}
+
+	printf("\n");
+	for(int i = 0; i < TEST_SIZE; i++){
+		shared_ptr<Node>n = heap.getMin();
+		heap.pop(nullptr);
+		printf("%u\n", n->kval);
+	}
+
+	return 0;
+}
+
+#endif
--- min_heap.h
+++ min_heap.h
@@ -1,6 +1,9 @@
 #ifndef _MIN_HEAP_H_
 #define _MIN_HEAP_H_
 
+#include <memory>
+using namespace std;
+
 #define MH_LEFT(n) (((n+1)*2) - 1)
 #define MH_RIGHT(n) ((n+1)*2)
 
@@ -10,14 +13,14 @@ class MinHeap
 public:
     MinHeap(int);
     ~MinHeap();
-    T* getMin() { return heap[0]; }
-    bool insert(T*);
-    int pop(T*);
+    shared_ptr<T> getMin() { return heap[0]; }
+    bool insert(shared_ptr<T>);
+    int pop(shared_ptr<T>);
     void makeHeap();
     void heapify(int);
 
 private:
-    T** heap = nullptr;
+    shared_ptr<T>* heap = nullptr;
     int cap = 0;
     int size = 0;
 };
